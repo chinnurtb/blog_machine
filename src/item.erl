5df5c54c4c9398a8cbe07e8ce26d769431cd8581
jamiiecb <jamiiecb@googlemail.com>
2010-04-14 16:50:23 +0000

    Fix incorrect wildcards in by_title
    Ignore-this: 8e4681cd9c04e7b39c0fa0fecbf83016
    
    darcs-hash:20100414165023-b18dd-ea2de1b5d48f8067f55b0145d8792ec165f9d349.gz

--- src/item.erl
+++ src/item.erl
@@ -1,5 +1,5 @@
 -module(item).
--export([start/0, insert/3, insert/4, insert_from_file/1, insert_from_dir/1, by_pubdate/1, all/0, ascending/1, descending/1, filter_tag/2, last/0]).
+-export([start/0, insert/3, insert/4, insert_from_file/1, insert_from_dir/1, by_pubdate/1, by_title/1, all/0, ascending/1, descending/1, filter_tag/2, last/0]).
 
 -include_lib("stdlib/include/qlc.hrl").
 -include_lib("kernel/include/file.hrl").
@@ -58,7 +58,7 @@ last() ->
   Item.
 
 by_title(Title) ->
-  {atomic, Items} = mnesia:transaction(fun () -> mnesia:match_object(#item{pubdate=' ', title=Title, tags=' ', body=' '}) end),
+  {atomic, Items} = mnesia:transaction(fun () -> mnesia:match_object(#item{pubdate='_', title=Title, tags='_', body='_'}) end),
   Items.
 
 all() ->
