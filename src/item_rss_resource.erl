73de87df0c0d819b116b4713633c82e366561d1e
jamiiecb <jamiiecb@googlemail.com>
2010-09-12 14:13:17 +0000

    Added rss feed
    Ignore-this: 99fdc45b20a733b888b6d5506fc97ba0
    
    darcs-hash:20100912141317-b18dd-20cc9cd2d02b3f204b9d4ee0a79e10de9535ca02.gz

---
+++ src/item_rss_resource.erl
@@ -0,0 +1,32 @@
+-module(item_rss_resource).
+-export([init/1, content_types_provided/2, to_rss/2]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+
+init([]) ->
+  item:start(),
+  {ok, undefined}.
+
+content_types_provided(ReqData, Context) ->
+   {[{"application/rss+xml",to_rss}], ReqData, Context}.
+
+handle_arg(Arg, Items) ->
+  case Arg of
+    {"tag", Tag} ->
+      item:filter_tag(Tag, Items);
+    _ -> Items
+  end. 
+
+get_items(Reqdata) ->
+  lists:foldl(
+    fun handle_arg/2, 
+    item:all(), 
+    wrq:req_qs(Reqdata)
+  ).
+
+to_rss(Reqdata, Context) ->
+  Items = get_items(Reqdata),
+  ok = erltl:compile("src/item_rss_template.et"),
+  Rss = item_rss_template:render(Items),
+  {Rss, Reqdata, Context}.
+
